4cd2586188789bc4358aec06c29dc3479a4fb461
Anshika Chauhan <chauhananshika533@gmail.com>
2025-07-29 17:04:43 +0530

    added contact page

--- my_project/settings.py
+++ my_project/settings.py
@@ -142,3 +142,10 @@ CLOUDINARY_STORAGE = {
     'API_KEY': config('CLOUDINARY_API_KEY'),
     'API_SECRET': config('CLOUDINARY_API_SECRET'),
 }
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'chauhananshika533@gmail.com'
+EMAIL_HOST_PASSWORD = 'hmnk vqbm vvfk kegj'  # App password (not Gmail login password)
