3d3e4c921912ea941d6f9b8d006faf7628814a5c
root <root@srv963258.hstgr.cloud>
2025-08-26 10:34:12 +0000

    Latest changes from company repo

--- testvenv311/bin/activate.fish
+++ testvenv311/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/root/Real_Estate-/testvenv311"
+set -gx VIRTUAL_ENV "/var/www/Real_Estate-/testvenv311"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
